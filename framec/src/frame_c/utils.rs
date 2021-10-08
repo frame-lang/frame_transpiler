20392a11c530b229007ec4031f133468d347b67b
Eric Walkingshaw <eric.walkingshaw@savant.com>
2021-10-08 10:55:06 -0700

    Make frame errors proper errors.

--- framec/src/frame_c/utils.rs
+++ framec/src/frame_c/utils.rs
@@ -1,5 +1,7 @@
 extern crate exitcode;
 use std::collections::HashMap;
+use std::error::Error;
+use std::fmt;
 
 pub struct Node {
     pub name: String,
@@ -139,6 +141,14 @@ pub(crate) mod frame_exitcode {
     /// Framepiler parse error exit
     pub const PARSE_ERR: FrameExitCode = 1;
     pub const DEFAULT_CONFIG_ERR: FrameExitCode = 2;
+
+    pub fn as_string(code: FrameExitCode) -> String {
+        match code {
+            PARSE_ERR => "Frame parse error".to_string(),
+            DEFAULT_CONFIG_ERR => "Default configuration error".to_string(),
+            _ => format!("Unknown error code {}", code),
+        }
+    }
 }
 
 pub struct RunError {
@@ -152,5 +162,33 @@ impl RunError {
             code,
             error: String::from(msg),
         }
+    }
+}
+
+impl fmt::Display for RunError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}: {}",
+            frame_exitcode::as_string(self.code),
+            self.error
+        )
+    }
+}
+
+impl fmt::Debug for RunError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}: {}",
+            frame_exitcode::as_string(self.code),
+            self.error
+        )
+    }
+}
+
+impl Error for RunError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        None
     }
 }
