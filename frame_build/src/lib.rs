1e612a2390dba23e27b45d4be7a04c3c79a63981
Eric Walkingshaw <eric.walkingshaw@savant.com>
2022-02-22 10:42:18 -0800

    Add more options, docs to frame_build.

--- frame_build/src/lib.rs
+++ frame_build/src/lib.rs
@@ -2,7 +2,7 @@
 //! assumed that these functions will be called from a `build.rs` file. This module uses Frame as a
 //! library, so `framec` does not need to be available on the path.
 
-use anyhow::Result;
+use anyhow::{Error, Result};
 use framec::frame_c::compiler::Exe;
 use std::path::{Path, PathBuf};
 use std::{env, fs};
@@ -11,27 +11,18 @@ use walkdir::WalkDir;
 // re-export `TargetLanguage` struct here since it's part of the `frame_build` interface
 pub use framec::frame_c::compiler::TargetLanguage;
 
-/// Configure the Frame build process.
+/// Configure the Frame build process. The methods associated with this struct are designed to be
+/// chained to override the default configuration.
 pub struct Config {
-    /// The output directory to store generated files.
-    ///
-    /// Default value is the default output directory for a `build.rs` script, which is obtained
-    /// from the `OUT_DIR` environment variable set by Cargo. If this library is used outside of
-    /// the context of a `build.rs` script, this value should be changed.
-    pub output_dir: PathBuf,
-
-    /// When looking for Frame files in the input directory, this is the maximum depth, where a
-    /// a max depth of `Some(1)` means to search only the immediate contents of the input
-    /// directory. For greater depths, subdirectories are traversed in a depth-first order.
-    ///
-    /// Default value is `None`, indicating that the entire tree of subdirectories will be
-    /// searched.
-    pub max_depth: Option<usize>,
-
-    /// Whether or not to follow symbolic links when traversing subdirectories.
-    ///
-    /// Default value is `false`.
-    pub follow_links: bool,
+    frame_config: Option<PathBuf>,
+    input_dir: PathBuf,
+    output_dir: PathBuf,
+    targets: Vec<TargetLanguage>,
+    input_filter: Box<dyn Fn(&Path) -> bool>,
+    max_depth: usize,
+    min_depth: usize,
+    follow_links: bool,
+    continue_on_error: bool,
 }
 
 impl Default for Config {
@@ -44,73 +35,190 @@ impl Config {
     /// Construct a new default configuration.
     pub fn new() -> Self {
         Config {
+            frame_config: None,
+            input_dir: PathBuf::from("src"),
             output_dir: PathBuf::from(env::var("OUT_DIR").unwrap()),
-            max_depth: None,
+            targets: vec![TargetLanguage::Rust],
+            input_filter: Box::new(|_| true),
+            max_depth: ::std::usize::MAX,
+            min_depth: 0,
             follow_links: false,
+            continue_on_error: false,
         }
     }
 
-    /// Set the output directory. This method can be chained with the constructor to override the
-    /// default configuration.
-    pub fn output_dir(mut self, dir: &Path) -> Self {
-        self.output_dir = dir.to_path_buf();
+    /// Set the path to a Frame `config.yaml` file to pass to Framec.
+    ///
+    /// If unset, Framec will look for the file in the current working directory.
+    pub fn frame_config(mut self, path: &Path) -> Self {
+        self.frame_config = Some(path.to_path_buf());
         self
     }
 
-    /// Set a maximum depth of traversal. This method can be chained with the constructor to
-    /// override the default configuration.
+    /// Set the directory to traverse, searching for `.frm` files.
+    ///
+    /// If unset, [frame_build] will search the project's `src` directory.
+    pub fn input_dir(mut self, path: &Path) -> Self {
+        self.input_dir = path.to_path_buf();
+        self
+    }
+
+    /// Set the root output directory to store generated files. Each generated file will be stored
+    /// at a relative path within this directory corresponding to the file's location in the input
+    /// directory.
+    ///
+    /// If unset, the build process will store generated files in the default output directory for
+    /// a `build.rs` script, which is obtained from the `OUT_DIR` environment variable set by
+    /// Cargo.
+    ///
+    /// If this library is used outside of the context of a `build.rs` script, this value should be
+    /// changed.
+    pub fn output_dir(mut self, path: &Path) -> Self {
+        self.output_dir = path.to_path_buf();
+        self
+    }
+
+    /// Set the list of target languages to compile each Frame file to using Framec.
+    ///
+    /// By default, the build process compiles each Frame file to only Rust.
+    pub fn set_targets(mut self, targets: &[TargetLanguage]) -> Self {
+        self.targets = targets.to_vec();
+        self
+    }
+
+    /// Add an additional target language to compile to.
+    ///
+    /// By default, the build process compiles each Frame file to Rust. If Rust output is not wanted,
+    /// use [Config::set_targets] to override the list of target languages rather than add to it.
+    pub fn add_target(mut self, target: TargetLanguage) -> Self {
+        self.targets.push(target);
+        self
+    }
+
+    /// Set a function that filters the Frame files found in the input directory based on their
+    /// paths. For the path of each Frame file found, if this function returns `true`, the file is
+    /// compiled into all of the target languages. If the function returns `false`, it is skipped.
+    pub fn include_only_if(mut self, filter: impl Fn(&Path) -> bool + 'static) -> Self {
+        self.input_filter = Box::new(filter);
+        self
+    }
+
+    /// Set a maximum depth to search for Frame files in the input directory. A max depth of `1`
+    /// means to search only the immediate contents of the input directory. For greater depths,
+    /// subdirectories are traversed in a depth-first order.
+    ///
+    /// By default, the max depth is effectively unbounded.
     pub fn max_depth(mut self, depth: usize) -> Self {
-        self.max_depth = Some(depth);
+        self.max_depth = depth;
         self
     }
 
-    /// Set the flag indicating whether to follow symbolic links. This method can be chained with
-    /// the constructor to override the default configuration.
-    pub fn follow_links(mut self, follow: bool) -> Self {
-        self.follow_links = follow;
+    /// Set a minimum depth to search for Frame files in the input directory. A min depth of `1`
+    /// would include the contents of the input directory, whereas a min depth of `2` would begin
+    /// with the contents of the input directory's sub-directories.
+    ///
+    /// By default, the min depth is `0`.
+    pub fn min_depth(mut self, depth: usize) -> Self {
+        self.max_depth = depth;
+        self
+    }
+
+    /// By default, the traversal of the input directory does not follow symbolic links. Calling
+    /// this method sets a flag indicating that symbolic links *should* be followed.
+    pub fn follow_links(mut self) -> Self {
+        self.follow_links = true;
+        self
+    }
+
+    /// By default, the build process halts if Framec panics or returns an error. Calling this
+    /// method sets a flag that changes this behavior to instead print an error message to `stderr`
+    /// and continue searching for and translating Frame files. This can be useful, for example,
+    /// when some Frame files are known to not translate to all target languages, and the failing
+    /// ones are not needed, or in test suites where some translations are expected to fail.
+    ///
+    /// Rather than setting this flag, consider also invoking [frame_build] multiple times with
+    /// different target languages and different configurations of [Self::include_only_if].
+    ///
+    /// Non-Frame errors (e.g. file I/O errors) will halt the build regardless of this setting.
+    pub fn continue_on_error(mut self) -> Self {
+        self.continue_on_error = true;
         self
     }
 }
 
-/// Run Framec on potentially several Frame files as part of building a Rust package. Intended to
-/// be called from a `build.rs` script. This function traverses a directory searching for `.frm`
-/// files, translating each one into potentially several different target languages.
+/// Run Framec on potentially several Frame files as part of building a Rust package. This function
+/// is intended to be called from a `build.rs` script. This function traverses a directory
+/// searching for `.frm` files, translating each one into potentially several different target
+/// languages.
 ///
-/// On success, returns a vector of paths to each of the files generated as a result of this
-/// process.
+/// The generated files are stored in an output directory at a relative position corresponding to
+/// where the files were found in the input directory. For example, a Frame specification found at
+/// `input_dir/a/b/sm.frm` might be translated into a Rust file `output_dir/a/b/sm.rs` and a
+/// PlantUML file `output_dir/a/b/sm.puml`.
 ///
-/// # Arguments
-/// * `frame_config` - Optional path to a `config.yaml` file to pass to Framec. If `None`, Framec
-///   will look for the file in the current working directory.
-/// * `build_config` - Used to configure aspects of this build function. See the documentation of
-///   the [Config] struct for options and their default values.
-/// * `input_dir` - The directory to search for `.frm` files.
-/// * `targets` - The target languages to compile each `.frm` file to using Framec.
-pub fn frame_build(
-    frame_config: Option<PathBuf>,
-    build_config: Config,
-    input_dir: PathBuf,
-    targets: Vec<TargetLanguage>,
-) -> Result<Vec<PathBuf>> {
+/// On success, this function returns a vector of paths to each of the generated files.
+///
+/// The build process is highly configurable. See the documentation for the [Config] struct.
+pub fn frame_build(config: Config) -> Result<Vec<PathBuf>> {
     let mut generated_files = Vec::new();
-    let exe = Exe::new();
 
-    for entry in WalkDir::new(&input_dir) {
+    let walk_dir = WalkDir::new(&config.input_dir)
+        .max_depth(config.max_depth)
+        .min_depth(config.min_depth)
+        .follow_links(config.follow_links);
+
+    for entry in walk_dir {
         let entry = entry?;
         let input_path = entry.path();
-        if input_path.extension().unwrap_or_default() == "frm" {
+        if input_path.extension().unwrap_or_default() == "frm" && (&config.input_filter)(input_path)
+        {
+            // tell Cargo this is a source file
             println!("cargo:rerun-if-changed={:?}", &input_path);
 
-            let local_path = input_path.strip_prefix(&input_dir)?;
-            let output_path = build_config.output_dir.join(local_path);
+            let local_path = input_path.strip_prefix(&config.input_dir)?;
+            let output_path = config.output_dir.join(local_path);
             fs::create_dir_all(output_path.parent().unwrap())?;
 
-            for target in &targets {
+            for target in &config.targets {
                 let mut target_output_path = output_path.clone();
                 target_output_path.set_extension(target.file_extension());
-                let output_content = exe.run_file(&frame_config, input_path, Some(*target))?;
-                fs::write(&target_output_path, output_content)?;
-                generated_files.push(target_output_path);
+
+                let frame_config = &config.frame_config;
+                let framec_result = std::panic::catch_unwind(move || {
+                    Exe::new().run_file(frame_config, input_path, Some(*target))
+                });
+
+                match framec_result {
+                    Ok(Ok(output_content)) => {
+                        // success, write the file
+                        fs::write(&target_output_path, output_content)?;
+                        generated_files.push(target_output_path);
+                    }
+                    Ok(Err(err)) => {
+                        // framec returned an error
+                        let msg = format!(
+                            "Framec errored while generating {:?}: {:?}",
+                            target_output_path, err
+                        );
+                        if config.continue_on_error {
+                            eprintln!("{}", msg);
+                        } else {
+                            return Err(Error::msg(msg));
+                        }
+                    }
+                    Err(err) => {
+                        // framec panicked
+                        let msg = format!(
+                            "Framec panicked while generating {:?}: {:?}",
+                            target_output_path, err
+                        );
+                        if config.continue_on_error {
+                            eprintln!("{}", msg);
+                        } else {
+                            return Err(Error::msg(msg));
+                        }
+                    }
+                }
             }
         }
     }
--- framec_tests/build.rs
+++ framec_tests/build.rs
@@ -1,9 +1,17 @@
 use anyhow::Result;
 use frame_build::*;
-use std::path::PathBuf;
 
 fn main() -> Result<()> {
-    let targets = vec![TargetLanguage::Rust];
-    frame_build(None, Config::new(), PathBuf::from("src"), targets)?;
+    let rust_config = Config::new();
+    frame_build(rust_config)?;
+
+    let smcat_config = Config::new()
+        .set_targets(&[TargetLanguage::Smcat])
+        .include_only_if(move |path| {
+            // the smcat backend panics if the state stack is used
+            !path.to_str().unwrap().contains("stack")
+        });
+    frame_build(smcat_config)?;
+
     Ok(())
 }
