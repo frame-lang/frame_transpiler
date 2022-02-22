380c8daa7be24a70b20f1cc0271dca10b9301c0c
Eric Walkingshaw <eric.walkingshaw@savant.com>
2022-02-22 11:58:31 -0800

    Make run a method, tons of docs.

--- framec_tests/build.rs
+++ framec_tests/build.rs
@@ -2,16 +2,15 @@ use anyhow::Result;
 use frame_build::*;
 
 fn main() -> Result<()> {
-    let rust_config = Config::new();
-    frame_build(rust_config)?;
+    FrameBuild::new().run()?;
 
-    let smcat_config = Config::new()
+    FrameBuild::new()
         .set_targets(&[TargetLanguage::Smcat])
         .include_only_if(move |path| {
             // the smcat backend panics if the state stack is used
             !path.to_str().unwrap().contains("stack")
-        });
-    frame_build(smcat_config)?;
+        })
+        .run()?;
 
     Ok(())
 }
