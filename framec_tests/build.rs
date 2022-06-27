896743d6c4e66f7e8de3a3f5b4f62573ef0646e9
deepaksharmaongraph <deepaksharma.ongraph@gmail.com>
2022-06-27 17:16:08 +0530

    add path struct for different location for frm files

--- framec_tests/build.rs
+++ framec_tests/build.rs
@@ -1,15 +1,43 @@
 use anyhow::Result;
 use frame_build::*;
+use std::path::PathBuf;
+
+struct Path{
+    golang: PathBuf,
+    javascript: PathBuf
+}
+
+impl Path{
+    fn get_path() -> Path {
+      Path {
+          golang : PathBuf::from("golang"),
+          javascript: PathBuf::from("javascript")
+      }
+    }
+}
+
+
 
 fn main() -> Result<()> {
+    let path = Path::get_path();
     FrameBuild::new().run()?;
-
     FrameBuild::new()
         .set_targets(&[TargetLanguage::Smcat])
         .include_only_if(move |path| {
             // the smcat backend panics if the state stack is used
             !path.to_str().unwrap().contains("stack")
         })
+        .run()?;
+
+    FrameBuild::new()
+        .set_targets(&[TargetLanguage::GoLang])
+        .input_dir(&path.golang)
+        .output_dir(&path.golang)
+        .run()?;
+        FrameBuild::new()
+        .set_targets(&[TargetLanguage::JavaScript])
+        .input_dir(&path.javascript)
+        .output_dir(&path.javascript)
         .run()?;
 
     Ok(())
